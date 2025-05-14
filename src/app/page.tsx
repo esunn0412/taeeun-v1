e7a3a46bacd60cac700bd4161687872c70ea8b63
TAEEUN KIM <taeeunk1208@gmail.com>
2025-05-15 00:16:33 +0900

    smooth scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,44 @@ export default function Home() {
   useEffect(() => {
     document.body.scrollTop = 0; // for safari
     document.documentElement.scrollTop = 0; // for chrome, firefox, ie and opera
+
+    document.querySelectorAll("nav a").forEach((anchor) => {
+      anchor.addEventListener(
+        "click",
+        function (this: HTMLAnchorElement, e: Event) {
+          e.preventDefault();
+          const href = this.getAttribute("href");
+          if (href) {
+            if (href === "#") {
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+            } else {
+              const element = document.querySelector(href);
+              if (element) {
+                element.scrollIntoView({
+                  behavior: "smooth",
+                });
+              }
+            }
+          }
+        }
+      );
+    });
+
+    // Cleanup function to remove event listeners
+    return () => {
+      document.querySelectorAll("nav a").forEach((anchor) => {
+        anchor.removeEventListener("click", () => {});
+      });
+    };
   }, []);
 
   return (
     <div>
       <Navbar />
-      <main className="max-w-screen-lg mx-auto px-4 mb-24 tracking-wide">
+      <main className="max-w-screen-lg mx-auto px-4 mb-24 tracking-wide scroll-smooth">
         <About />
 
         <Divider />

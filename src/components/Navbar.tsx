294c2edbc537c7548f133539b46eb7abba95a56b
TAEEUN KIM <taeeunk1208@gmail.com>
2025-05-15 01:43:24 +0900

    navbar animations and hover effect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,31 @@
 const Navbar = () => {
   return (
-    <nav className="tracking-widest flex justify-between items-center py-6 px-10 fixed top-0 left-0 right-0 z-50 bg-opacity-90 backdrop-blur-sm">
-      <a className="cursor-pointer font-bold" href="#">
-        Taeeun Kim
+    <nav className="transform-all duration-300 ease-in-out tracking-widest flex justify-between items-center py-6 px-10 fixed top-0 left-0 right-0 z-50 backdrop-blur-sm">
+      <a
+        href="#"
+        className="cursor-pointer font-bold flex space-x-0.5 group transition-transform duration-200 ease-out hover:scale-105"
+      >
+        {"Taeeun Kim".split("").map((char, i) => (
+          <span
+            key={i}
+            className="inline-block transition-colors duration-200 ease out group-hover:text-[var(--highlight)]"
+            style={{
+              transitionDelay: `${i * 50}ms`,
+            }}
+          >
+            {char === " " ? "\u00A0" : char}
+          </span>
+        ))}
       </a>
+
       <div className="cursor-pointer flex gap-12">
-        <a className="hover:text-gray-400" href="#">
+        <a className="hover:text-[var(--highlight)]" href="#">
           About
         </a>
-        <a className="hover:text-gray-400" href="#experience">
+        <a className="hover:text-[var(--highlight)]" href="#experience">
           Experience
         </a>
-        <a className="hover:text-gray-400" href="#projects">
+        <a className="hover:text-[var(--highlight)]" href="#projects">
           Projects
         </a>
       </div>
